6cb51fdf7db31a021b1de5d5cb63836ca707dca5
Jeremy Kendall <jeremy@jeremykendall.net>
2012-12-27 07:50:04 -0600

    Adds composer.json and PHPUnit dependencey

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,29 +6,7 @@ ini_set('display_startup_errors', 1);
 
 date_default_timezone_set('America/Chicago');
 
-set_include_path(implode(PATH_SEPARATOR, array(
-    get_include_path(),
-    realpath(__DIR__ . '/../library')
-)));
-
-define('PROJECT_ROOT_DIR', realpath(__DIR__ . '/..'));
-
-function autoload($className)
-{
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strrpos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-    require $fileName;
-}
-
-spl_autoload_register('autoload');
+require_once __DIR__ . '/../vendor/autoload.php';
 
 function d($expression)
 {
