47217317170a5f932d3da602853c7a97e91bf603
Jeremy Kendall <jeremy@jeremykendall.net>
2012-12-27 00:46:08 -0600

    Completes second #12tdds, NumberNames

--- library/Tdd/NumberNames.php
+++ library/Tdd/NumberNames.php
@@ -4,53 +4,44 @@ namespace Tdd;
 
 class NumberNames
 {
-    protected $unique;
+    protected $unique = array(
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen',
+        'twenty',
+        30 => 'thirty',
+        40 => 'forty',
+        50 => 'fifty',
+        60 => 'sixty',
+        70 => 'seventy',
+        80 => 'eighty',
+        90 => 'ninety'
+    );
 
-    protected $names;
-
-    public function __construct()
-    {
-        $this->unique = array(
-            'zero',
-            'one',
-            'two',
-            'three',
-            'four',
-            'five',
-            'six',
-            'seven',
-            'eight',
-            'nine',
-            'ten',
-            'eleven',
-            'twelve',
-            'thirteen',
-            'fourteen',
-            'fifteen',
-            'sixteen',
-            'seventeen',
-            'eighteen',
-            'nineteen',
-            'twenty',
-            30 => 'thirty',
-            40 => 'fourty',
-            50 => 'fifty',
-            60 => 'sixty',
-            70 => 'seventy',
-            80 => 'eighty',
-            90 => 'ninety'
-        );
-
-        $this->names = array(
-            1 => 'hundred',
-            2 => 'thousand',
-            3 => 'million',
-            4 => 'billion'
-            5 => 'trillion'
-        );
+    protected $units = array(
+        2 => 'thousand',
+        3 => 'million'
+    );
 
     /**
-     * Converts an integer to it's english language equivalent.
+     * Converts an integer to its English language equivalent.
      *
      * Example: 310 would become three hundred and ten.
      *
@@ -58,9 +49,66 @@ class NumberNames
      */
     public function convert($integer)
     {
-        $numbers = str_split($integer, 3);
+        if ($integer == 0) {
+            return 'zero';
+        }
 
-        return 'ninety nine';
+        $name = array();
+        $numbers = explode(',', number_format($integer));
+        $count = count($numbers);
+
+        foreach ($numbers as $number) {
+            $padded = sprintf('%03d', $number);
+
+            $temp = $this->getHundreds($padded);
+            $temp .= $this->getTens(substr($padded, 1));
+
+            if ($count > 1) {
+                $temp .= ' ' . $this->units[$count];
+            }
+
+            $count--;
+
+            if ($temp) {
+                $name[] = $temp;
+            }
+        }
+
+        return implode(', ', $name);
+    }
+
+    protected function getHundreds($hundreds)
+    {
+        $string = null;
+
+        if ($hundreds[0]) {
+            $string = $this->unique[$hundreds[0]] . ' hundred';
+        }
+
+        if ($string && ltrim(substr($hundreds, 1), '0')) {
+            $string .= ' and ';
+        }
+
+        return $string;
+    }
+
+    public function getTens($tens)
+    {
+        if (array_key_exists($tens, $this->unique)) {
+            return $this->unique[$tens];
+        }
+
+        $temp = null;
+        
+        if ($tens[0] > 0) {
+            $temp .= $this->unique[$tens[0] * 10] . ' ';
+        }
+        
+        if ($tens[1] > 0) {
+            $temp .= $this->unique[$tens[1]];
+        }
+
+        return $temp;
     }
 
 }
--- tests/library/Tdd/NumberNamesTest.php
+++ tests/library/Tdd/NumberNamesTest.php
@@ -18,7 +18,7 @@ class NumberNamesTest extends \PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        $this->object = new NumberNames;
+        $this->object = new NumberNames();
     }
 
     /**
@@ -31,7 +31,6 @@ class NumberNamesTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers Tdd\NumberNames::convert
      * @dataProvider convertDataProvider
      */
     public function testConvert($integer, $string)
@@ -42,8 +41,21 @@ class NumberNamesTest extends \PHPUnit_Framework_TestCase
     public function convertDataProvider()
     {
         return array(
+            array(0, 'zero'),
+            array(5, 'five'),
+            array(12, 'twelve'),
+            array(67, 'sixty seven'),
             array(99, 'ninety nine'),
-            array(27, 'twenty seven')
+            array(300, 'three hundred'),
+            array(804, 'eight hundred and four'),
+            array(220, 'two hundred and twenty'),
+            array(645, 'six hundred and forty five'),
+            array(118, 'one hundred and eighteen'),
+            array(1000, 'one thousand'),
+            array(1501, 'one thousand, five hundred and one'),
+            array(12609, 'twelve thousand, six hundred and nine'),
+            array(512607, 'five hundred and twelve thousand, six hundred and seven'),
+            array(43112603, 'forty three million, one hundred and twelve thousand, six hundred and three')
         );
     }
 }
