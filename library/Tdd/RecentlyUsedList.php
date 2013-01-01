98ff74ba698e59a694b5eb3e1010d7105a060576
Jeremy Kendall <jeremy@jeremykendall.net>
2013-01-01 14:06:01 -0600

    12TDDs Day 6: Recently-Used List

---
+++ library/Tdd/RecentlyUsedList.php
@@ -0,0 +1,207 @@
+<?php
+
+/**
+ * 12 TDDs of Christmas
+ *
+ * @link      http://github.com/jeremykendall/12-tdds-of-christmas for the canonical source repository
+ * @copyright Copyright (c) 2012 Jeremy Kendall (http://about.me/jeremykendall)
+ * @license   http://github.com/jeremykendall/12-tdds-of-christmas/blob/master/LICENSE MIT License
+ * @see       http://www.wiredtothemoon.com/2012/12/12-tdds-of-christmas/ 12 TDDs of Chrismas blog post
+ */
+
+namespace Tdd;
+
+/**
+ * RecentlyUsedList (LIFO)
+ *
+ * @package TwelveTddsOfChristmas\Day6
+ */
+class RecentlyUsedList implements \ArrayAccess, \Countable, \Iterator
+{
+    /**
+     * @var int Current array position
+     */
+    private $position;
+
+    /**
+     * @var array LIFO list
+     */
+    protected $list;
+
+    /**
+     * Public constructor
+     */
+    public function __construct()
+    {
+        $this->position = 0;
+        $this->list = array();
+    }
+
+    /**
+     * Gets all list items
+     *
+     * @return array
+     */
+    public function getList()
+    {
+        return $this->list;
+    }
+
+    /**
+     * Pushes item onto LIFO stack
+     *
+     * @param mixed $item Item to push onto stack
+     */
+    public function push($item)
+    {
+        if (is_null($item) || $item == '') {
+            throw new \InvalidArgumentException('Cannot add null values or empty strings.');
+        }
+
+        if ($this->search($item) !== false) {
+            $this->offsetUnset($this->search($item));
+        }
+
+        array_unshift($this->list, $item);
+    }
+
+    /**
+     * Is stack empty
+     *
+     * @return bool True if empty, false if not empty
+     */
+    public function isEmpty()
+    {
+        return empty($this->list);
+    }
+
+    /**
+     * Peeks at top value
+     *
+     * @return mixed Top item on stack
+     */
+    public function top()
+    {
+        return $this->list[0];
+    }
+
+    /**
+     * Sets value at offset
+     *
+     * @param int   $offset Offset
+     * @param mixed $value  Value to set at offset
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->push($value);
+        } else {
+            if ($this->search($value) !== false) {
+                $this->offsetUnset($this->search($value));
+            }
+
+            array_splice($this->list, $offset, 0, array($value));
+            //$this->list[$offset] = $value;
+        }
+    }
+
+    /**
+     * Tests whether or not offset exists
+     *
+     * @param  int  $offset Offset to test
+     * @return bool True if offset exists, false otherwise
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->list[$offset]);
+    }
+
+    /**
+     * Unsets item at offset
+     *
+     * @param int $offset Offset to unset
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->list[$offset]);
+    }
+
+    /**
+     * Gets item at offset
+     *
+     * @param  int   $offset Offset
+     * @return mixed Item value at offset
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->list[$offset]) ? $this->list[$offset] : null;
+    }
+
+    /**
+     * Counts items in stack
+     *
+     * @return int Count of items in stack
+     */
+    public function count()
+    {
+        return count($this->list);
+    }
+
+    /**
+     * Resets stack counter to 0
+     */
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+
+    /**
+     * Returns item at current index
+     *
+     * @return mixed Item at current index
+     */
+    public function current()
+    {
+        return $this->list[$this->position];
+    }
+
+    /**
+     * Returns current array position
+     *
+     * @return int Current array position
+     */
+    public function key()
+    {
+        return $this->position;
+    }
+
+    /**
+     * Increments array position
+     */
+    public function next()
+    {
+        ++$this->position;
+    }
+
+    /**
+     * Tests if current array position is set
+     *
+     * @return bool True if position is set, false otherwise
+     */
+    public function valid()
+    {
+        return isset($this->list[$this->position]);
+    }
+
+    /**
+     * Searchs list for item
+     *
+     * @param  mixed     $item Needle
+     * @return int|false Offset if item found, false otherwise
+     */
+    protected function search($item)
+    {
+        return array_search($item, $this->list);
+    }
+
+}
---
+++ tests/library/Tdd/RecentlyUsedListTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Tdd;
+
+class RecentlyUsedListTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var RecentlyUsedList
+     */
+    protected $list;
+
+    protected function setUp()
+    {
+        $this->list = new RecentlyUsedList();
+        $this->list->push('first');
+        $this->list->push('second');
+        $this->list->push('third');
+    }
+
+    public function testListInitiallyEmpty()
+    {
+        $empty = new RecentlyUsedList();
+        $this->assertTrue($empty->isEmpty());
+    }
+
+    public function testLastInIsFirstOut()
+    {
+        $this->assertEquals('third', $this->list->top());
+    }
+
+    public function testAddingDuplicateItemMovesItemToTopOfList()
+    {
+        $this->assertEquals(3, count($this->list));
+
+        $this->list->push('second');
+
+        // Ensure 'second' is the first item in the list
+        $this->assertEquals('second', $this->list->top());
+        // Ensure that second only shows up in the list once
+        $this->assertEquals(1, count(array_keys($this->list->getList(), 'second')));
+        // Ensure that there are still only 3 items in the list
+        $this->assertEquals(3, count($this->list));
+    }
+
+    public function testAddingDuplicateItemWithOffsetSetMovesItemToTopOfList()
+    {
+        $this->assertEquals(3, count($this->list));
+
+        $this->list->offsetSet(null, 'second');
+
+        // Ensure 'second' is the first item in the list
+        $this->assertEquals('second', $this->list->top());
+        // Ensure that second only shows up in the list once
+        $this->assertEquals(1, count(array_keys($this->list->getList(), 'second')));
+        // Ensure that there are still only 3 items in the list
+        $this->assertEquals(3, count($this->list));
+    }
+
+    public function testAddingDuplicateItemWithOffsetSet()
+    {
+        $this->assertEquals(3, count($this->list));
+
+        $this->list->offsetSet(2, 'second');
+        $this->assertEquals('second', $this->list->offsetGet(2));
+
+        // Ensure 'third' is still the first item in the list
+        $this->assertEquals('third', $this->list->top());
+        // Ensure that second only shows up in the list once
+        $this->assertEquals(1, count(array_keys($this->list->getList(), 'second')));
+        // Ensure that there are still only 3 items in the list
+        $this->assertEquals(3, count($this->list));
+    }
+    public function testAddingNullValueThrowsException()
+    {
+        $this->setExpectedException('\InvalidArgumentException', 'Cannot add null values or empty strings.');
+        $this->list->push(null);
+    }
+
+    public function testAddingEmptyStringThrowsException()
+    {
+        $this->setExpectedException('\InvalidArgumentException', 'Cannot add null values or empty strings.');
+        $this->list->push('');
+    }
+
+    public function testIteratorIsLifo()
+    {
+        $expected = array('third', 'second', 'first');
+        $i = 0;
+        foreach($this->list as $item) {
+            $this->assertEquals($expected[$i], $item);
+            $i++;
+        }
+    }
+
+    /**
+     * This test ensures the ArrayAccess methods are working as expected
+     *
+     * offsetSet() has some logic behind it, so that is tested in other tests as 
+     * well as here.
+     */
+    public function testArrayAccessMethods()
+    {
+        $this->assertEquals('third', $this->list[0]);
+        $this->assertEquals(3, count($this->list));
+
+        unset($this->list[0]);
+        $this->assertEquals(2, count($this->list));
+
+        $this->list->offsetSet(2, 'new');
+        $this->assertEquals('new', $this->list[2]);
+        $this->assertEquals(3, count($this->list));
+        
+        $this->list->offsetSet(null, 'zing!');
+        $this->assertEquals(4, count($this->list));
+        $this->assertEquals('zing!', $this->list->top());
+        
+        $this->assertTrue($this->list->offsetExists(2));
+        
+        $this->list->rewind();
+        $this->assertEquals(0, $this->list->key());
+        $this->list->next();
+        $this->assertEquals(1, $this->list->key());
+    }
+
+}
